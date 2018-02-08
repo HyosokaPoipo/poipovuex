acec53f5616d409035ed6e8c937d798fbfe3e825
Maryadi <maryadi@astrnt.co>
2018-02-09 06:21:20 +0700

    nambain main&biju team.vue

---
+++ components/bijuu_team.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="bijuu-team">
+    <h1> Akatsuki Bijuu Team</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ components/main_team_member.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="main-team">
+  <h1> Akatsuki Main Team </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
