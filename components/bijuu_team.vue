3898a907a7b741bd00aa229a21b6cd782563f95e
Maryadi <maryadi@astrnt.co>
2018-02-09 11:10:36 +0700

    add data to bijuu team

--- components/bijuu_team.vue
+++ components/bijuu_team.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="bijuu-team">
     <h1> Akatsuki Bijuu Team</h1>
+    <ul>
+      <li v-for="team in bijuu_teams">
+          <div>Name : {{team.name}}, Position : {{team.position}}  </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-
+  computed: {
+    bijuu_teams() {
+      return this.$store.state.bijuu_teams;
     }
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -9,6 +9,13 @@ export const store = new Vuex.Store({
       {name: 'Painz', position: 'leader', value: 99},
       {name: 'Konan', position: 'vice leader', value: 93},
       {name: 'Tobi', position: 'secret leader', value: 97}
+    ],
+    bijuu_teams: [
+      {name: 'Itachi', position: 'bla bla bla', value: 87},
+      {name: 'Hisoka', position: 'commander 1', value: 89},
+      {name: 'Kisame', position: 'itachi\'s backup ', value: 77},
+      {name: 'Sasory', position: 'winger', value: 73},
+      {name: 'Poipo', position: 'captain', value: 91}
     ]
   },
   getters: {
