68d245edbd403028b5f42f972d843d599b85901e
Maryadi <maryadi@astrnt.co>
2018-02-09 09:35:21 +0700

    integrating vue files to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
-
+    <main-team></main-team>
+    <bijuu-team></bijuu-team>
   </div>
 </template>
 
 <script>
+
+import MainTeam from '../components/main_team_member.vue';
+import BijuuTeam from '../components/bijuu_team.vue';
+
 export default {
+  components: {
+    'main-team' : MainTeam,
+    'bijuu-team' : BijuuTeam
+  },
   name: 'app',
   data () {
     return {
